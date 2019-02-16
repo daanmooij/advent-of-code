7351ff23370a88706df553291986ec44cd0dfec9
Daan Mooij <daanmooij@gmail.com>
2019-02-16 20:19:25 +0100

    complete day 1 part 2

--- day-1.php
+++ day-1.php
@@ -2,11 +2,32 @@
 
 $file = fopen("changes.txt", "r");
 $frequency = 0;
+$changes = [];
 
 while (!feof($file)) {
-    $frequency += intval(fgets($file));
+    $change = intval(fgets($file));
+    $changes[] = $change;
+    $frequency += $change;
 }
 
-echo "Result: " . $frequency . "\n";
-
 fclose($file);
+
+echo "Result part one: " . $frequency . "\n";
+
+$changes = array_filter($changes);
+
+$memory = [0];
+$frequency = 0;
+
+while (true) {
+    foreach ($changes as $change) {
+        $frequency += $change;
+
+        if (in_array($frequency, $memory)) {
+            echo 'First frequency reached twice: ' . $frequency . PHP_EOL;
+            break 2;
+        }
+
+        $memory[] = $frequency;
+    }
+}
