57bbce865f2b7b68088d5cb898d63eb64aa533c0
Daan Mooij <daanmooij@gmail.com>
2020-03-11 23:02:23 +0100

    complete day 1

---
+++ src/Year2018/Day1.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace DaanMooij\AdventOfCode\Year2018;
+
+class Day1
+{
+    /**
+     * @var array
+     */
+    private $changes;
+
+    /**
+     * @param array $changes
+     */
+    public function __construct(array $changes)
+    {
+        $this->changes = $changes;
+    }
+
+    /**
+     * @param string $filePath
+     */
+    public static function fromFile(string $filePath)
+    {
+        $changes = [];
+
+        $file = fopen(__DIR__ . $filePath, "r");
+        while (!feof($file)) {
+            $changes[] = intval(fgets($file));
+        }
+        fclose($file);
+
+        return new Day1($changes);
+    }
+
+    /**
+     * @param int $frequency
+     * @return int
+     */
+    public function getFrequency(int $frequency): int
+    {
+        foreach ($this->changes as $change) {
+            $frequency += $change;
+        }
+
+        return $frequency;
+    }
+
+    /**
+     * @param int $frequency
+     * @return int
+     */
+    public function getFrequencyReachedTwice(int $frequency): int
+    {
+        $memory = [$frequency => 1];
+
+        $found = false;
+        while (!$found) {
+            foreach ($this->changes as $change) {
+                $frequency += $change;
+
+                if (isset($memory[$frequency])) {
+                    $found = true;
+                    break;
+                }
+
+                $memory[$frequency] = 1;
+            }
+        }
+
+        return $frequency;
+    }
+}
---
+++ tests/unit/Day1Test.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace DaanMooij\AdventOfCode\Year2018;
+
+use PHPUnit\Framework\TestCase;
+
+class Day1Test extends TestCase
+{
+    /** 
+     * @test 
+     * @dataProvider changes_with_resulting_frequency
+     */
+    public function it_returns_frequency_from_given_changes(array $changes, int $result): void
+    {
+        $day1 = new Day1($changes);
+
+        $this->assertEquals($result, $day1->getFrequency(0));
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_frequency_from_changes_in_file(): void
+    {
+        $day1 = Day1::fromFile('/files/changes.txt');
+
+        $result = $day1->getFrequency(0);
+
+        $this->assertIsInt($result);
+    }
+
+    /**
+     * @test
+     * @dataProvider changes_with_first_frequency_reached_twice
+     */
+    public function it_returns_first_frequency_reached_twice_from_given_changes(array $changes, int $result): void
+    {
+        $day1 = new Day1($changes);
+
+        $this->assertEquals($result, $day1->getFrequencyReachedTwice(0));
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_first_frequency_reached_twice_from_changes_in_file(): void
+    {
+        $day1 = Day1::fromFile('/files/changes.txt');
+
+        $result = $day1->getFrequencyReachedTwice(0);
+
+        $this->assertIsInt($result);
+    }
+
+    public function changes_with_resulting_frequency(): array
+    {
+        return [
+            [['+1', '+1', '+1'], 3],
+            [['+1', '+1', '-2'], 0],
+            [['-1', '-2', '-3'], -6],
+            [['+1', '-2', '+3', '+1'], 3]
+        ];
+    }
+
+    public function changes_with_first_frequency_reached_twice(): array
+    {
+        return [
+            [['+1', '-1'], 0],
+            [['+3', '+3', '+4', '-2', '-4'], 10],
+            [['-6', '+3', '+8', '+5', '-6'], 5],
+            [['+7', '+7', '-2', '-7', '-4'], 14]
+        ];
+    }
+}
