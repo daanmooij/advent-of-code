e1212a058dddee31cc55756dbfebb3959ae9a399
Daan Mooij <daanmooij@gmail.com>
2020-12-20 20:13:02 +0100

    initialise year 2015 day 3

---
+++ src/Year2015/Day3/Day3.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace DaanMooij\AdventOfCode\Year2015\Day3;
+
+use DaanMooij\AdventOfCode\Day;
+
+class Day3 implements Day
+{
+    public function loadInput(): void
+    {
+        
+    }
+
+    public function solve(): void
+    {
+        
+    }
+}
